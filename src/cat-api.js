f25ba190548c287aa6265342d9aacd92a7926cae
Rhilim <arhilim@gmail.com>
2023-06-29 16:18:46 -0500

    update select element

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,33 @@
-
 import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'api_key=live_qVIX3pve3cDaT0OEOvS7AYCE7mYAILrQlRPd9doijm9Adi6P7vAXPc3SQaY8Qvei';
 
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+const select = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 
-
-const fetchBreeds = breed =>
-  fetch(`${BASE_URL}`).then(res => {
-    return res.json();
-  });
+fetchBreeds()
+  .then(addOptionELtoSelect)
+  .catch(error => console.log(error));
 
   export default fetchBreeds;
+
+function fetchBreeds() {
+  return fetch(`${BASE_URL}`).then(res => {
+    if (!res.ok) {
+      throw Error(res.status);
+    }
+    return res.json();
+  });
+}
+
+function addOptionELtoSelect(data) {
+  data.map(breed => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.text = breed.name;
+    //  console.log(option);
+    return select.appendChild(option);
+  });
+}
+
--- src/index.js
+++ src/index.js
@@ -3,25 +3,32 @@
 // Use it as the 'x-api-key' header when making any request to the API, or by adding as a query string parameter e.g.
 // 'api_key=live_qVIX3pve3cDaT0OEOvS7AYCE7mYAILrQlRPd9doijm9Adi6P7vAXPc3SQaY8Qvei'
 // https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME
-
+import fetchBreeds from './cat-api';
 import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'api_key=live_qVIX3pve3cDaT0OEOvS7AYCE7mYAILrQlRPd9doijm9Adi6P7vAXPc3SQaY8Qvei';
 
-// const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
 
-// fetch(`${BASE_URL}`)
-//   .then(res => {
-//     // if (!res.ok) {
-//     //   throw new Error(res.status);
-//     // }
-//     return res.json();
-//   })
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+const select = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+// console.log(select);
+// console.log(catInfo);
+ 
+// fetchBreeds.then(data =>
+//  const options = data.map(breed => {
+//    const option = document.createElement('option');
+//    option.value = obj.id;
+//     option.text = obj.name;
+//     return option;
+//  }
+  
+//  ))
 
 
+  // var options = arrayOfObjects.map(function(obj) {
+  //   var option = document.createElement('option');
+  //   option.value = obj.id;
+  //   option.text = obj.name;
+  //   return option;
+  // })
