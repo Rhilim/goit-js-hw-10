b6609fb4036a6f7bcfc8800c27d76c23dff43b00
Rhilim <arhilim@gmail.com>
2023-07-01 17:18:48 -0500

    final

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,33 +1,53 @@
-import axios from 'axios';
-axios.defaults.headers.common['x-api-key'] =
-  'api_key=live_qVIX3pve3cDaT0OEOvS7AYCE7mYAILrQlRPd9doijm9Adi6P7vAXPc3SQaY8Qvei';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+// new SlimSelect({
+//   select: '#select'
+// });
 
-const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
-const select = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
+export const loader = document.querySelector('.loader');
 
-fetchBreeds()
-  .then(addOptionELtoSelect)
-  .catch(error => console.log(error));
+const breedSelect = document.querySelector('.breed-select');
+export { breedSelect };
+const errorMsg = document.querySelector('.error');
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const API_KEY =
+  'live_qVIX3pve3cDaT0OEOvS7AYCE7mYAILrQlRPd9doijm9Adi6P7vAXPc3SQaY8Qvei';
 
-  export default fetchBreeds;
+export function fetchBreeds() {
+  loader.textContent = '';
+  fetch(`${BASE_URL}breeds?api_key=${API_KEY}`)
+    .then(response => {
+      if (!response.ok) {
+        Notify.failure(`${errorMsg.textContent}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // console.log(data);
+      createAllOptions(data);
 
-function fetchBreeds() {
-  return fetch(`${BASE_URL}`).then(res => {
-    if (!res.ok) {
-      throw Error(res.status);
-    }
-    return res.json();
-  });
+      errorMsg.textContent = '';
+      loader.style.display = 'none';
+    })
+    .catch(error => {
+      // console.log(error);
+      loader.style.display = 'none';
+    });
 }
 
-function addOptionELtoSelect(data) {
-  data.map(breed => {
-    const option = document.createElement('option');
-    option.value = breed.id;
-    option.text = breed.name;
-    //  console.log(option);
-    return select.appendChild(option);
-  });
+function createAllOptions(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    let value = arr[i].id;
+    let text = arr[i].name;
+    const optionsElements = document.createElement('option');
+    optionsElements.value = value;
+    optionsElements.textContent = text;
+    breedSelect.appendChild(optionsElements);
+  }
 }
 
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  ).then(response => response.json());
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,113 @@
-// Here's your API key:
-// live_qVIX3pve3cDaT0OEOvS7AYCE7mYAILrQlRPd9doijm9Adi6P7vAXPc3SQaY8Qvei
-// Use it as the 'x-api-key' header when making any request to the API, or by adding as a query string parameter e.g.
-// 'api_key=live_qVIX3pve3cDaT0OEOvS7AYCE7mYAILrQlRPd9doijm9Adi6P7vAXPc3SQaY8Qvei'
-// https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME
-import fetchBreeds from './cat-api';
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+// new SlimSelect({
+//   select: '#select',
+// });
+
 axios.defaults.headers.common['x-api-key'] =
-  'api_key=live_qVIX3pve3cDaT0OEOvS7AYCE7mYAILrQlRPd9doijm9Adi6P7vAXPc3SQaY8Qvei';
+  'live_6oipNaEf4MhA3t1QKH9rU8tuRomBydjiP5ZsBkmsecdwgBpHv8VKGluJCVsYRtAH';
+import {
+  loader,
+  fetchCatByBreed,
+  fetchBreeds,
+  breedSelect,
+} from './cat-api.js';
 
-const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+fetchBreeds();
 
-const select = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
-// console.log(select);
-// console.log(catInfo);
- 
-// fetchBreeds.then(data =>
-//  const options = data.map(breed => {
-//    const option = document.createElement('option');
-//    option.value = obj.id;
-//     option.text = obj.name;
-//     return option;
-//  }
-  
-//  ))
+const catContainer = document.querySelector('.cat-info');
 
+breedSelect.addEventListener('change', onSelectView);
 
-  // var options = arrayOfObjects.map(function(obj) {
-  //   var option = document.createElement('option');
-  //   option.value = obj.id;
-  //   option.text = obj.name;
-  //   return option;
-  // })
+function onSelectView() {
+  const breedId = selectedBreeds();
+  const isContent = document.querySelector('.img-cat');
+
+  if (isContent) {
+    clearCatContainer();
+  }
+
+  showLoadingMessage();
+
+  fetchCatByBreed(breedId)
+    .then(markUp)
+    .catch(showError)
+    .finally(hideLoadingMessage);
+}
+
+function selectedBreeds() {
+  const selectedValue = breedSelect.options[breedSelect.selectedIndex];
+  const selectedText = selectedValue.textContent;
+  const selectedId = selectedValue.value;
+
+  return selectedId;
+}
+
+function showLoadingMessage() {
+  loader.style.display = 'block';
+  Notify.success('Here is selected breed!');
+}
+
+function markUp(arr) {
+  let imgUrl = arr.map(link => link.url);
+  let catDesc = arr.map(link => link.breeds[0].description);
+  let catTemp = arr.map(link => link.breeds[0].temperament);
+  let catName = arr.map(link => link.breeds[0].name);
+
+  const catMarkUp = `
+      <table class='table'>
+      <tr>
+      <td>
+      <img class="img-cat" src="${imgUrl}" width="440" height="400" loading="lazy">
+      </td>
+      <td class="text-cell">
+      <div class="text-desc">
+      <h1 class="cat-title" style='font-family: Helvetica; font-size: 60px;
+  background: -webkit-linear-gradient(#eee, #333);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;'>${catName}</h1>
+        <p class="cat-info" style='font-family: Helvetica; color: #606470'><b>Description: </b>${catDesc}</p>
+        <p class="cat-info" style='font-family: Helvetica; color: #606470'><b>Temperament: </b>${catTemp}</p>
+      </div>
+      </td>
+      </tr>
+      </table>
+    `;
+
+  catContainer.insertAdjacentHTML('beforeend', catMarkUp);
+}
+
+function showError() {
+  Notify.failure('Oops! Something went wrong! Try reloading the page!');
+}
+
+function hideLoadingMessage() {
+  loader.style.display = 'none';
+}
+
+function clearCatContainer() {
+  const children = Array.from(catContainer.children);
+
+  children.forEach(child => {
+    catContainer.removeChild(child);
+  });
+}
+
+// STYLES
+
+const bodyEl = document.querySelector('body');
+bodyEl.style.cssText =
+  'background-image: linear-gradient(to right, #FFEFBA,  #FFFFFF); text-align: center';
+
+breedSelect.style.cssText =
+  'text-align: center; background-color: #FFEFBA; font-weight: 600; font-family: Helvetica'; 
+
+catContainer.style.cssText = 'text-align: left; border: 1px solid #FFEFBA';
+
+loader.style.cssText = `width: 96px; box-sizing: content-box; height: 48px; background: #FFF; border-color: #de3500; border-style: solid; border-width: 2px 2px 50px 2px; border-radius: 100%; position: relative; animation: 3s yinYang linear infinite`;
+loader.style.cssText = `:before:  content: ""; position: absolute; top: 50%; left: 0; background: #FFF; border: 18px solid #de3500; border-radius: 100%; width: 12px; height: 12px; box-sizing: content-box`;
+loader.style.cssText = `:after: content: ""position: absolute-top: 50%left: 50%background: #de3500border: 18px solid #FFF; border-radius: 100%; width: 12px; height: 12px box-sizing: content-box`;
+loader.style.cssText = `@keyframes yinYang {
+  100%{transform: rotate(360deg)}
+}`
